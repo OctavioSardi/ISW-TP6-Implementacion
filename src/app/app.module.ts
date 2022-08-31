84fc4d8265713ccc1a973180379eb5213097c0ae
OctavioSardi <octavio.sardi@protonmail.com>
2022-08-31 13:13:07 -0300

    5 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, MenuComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
